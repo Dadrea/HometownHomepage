374c8d16ea52f605119def144644c02534f2a1bb
Dadrea <rm4982@gmail.com>
2025-01-09 22:13:08 -0500

    design changes and added linkedIn connection

---
+++ main.js
@@ -0,0 +1,11 @@
+import './styles.css'
+import linkedInLogo from "/linkedin.svg"
+
+document.querySelector('#contactMe').innerHTML = `
+  <div>
+    <a href="https://www.linkedin.com/in/ryan-martin-8909ab126/" target="_blank">
+      <img src="${linkedInLogo}" class="logo linkedIn" alt="LinkedIn logo" />
+    </a>
+    
+  </div>
+  `
