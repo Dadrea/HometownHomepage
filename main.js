2cf06abe7f6b7795be9d74481c50177bfbe41d9e
Dadrea <rm4982@gmail.com>
2025-01-10 09:16:28 -0500

    design changes

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 import './styles.css'
 import linkedInLogo from "/linkedin.svg"
+import githubLogo from "/gihub-mark-white.svg"
 
 document.querySelector('#contactMe').innerHTML = `
   <div>
     <a href="https://www.linkedin.com/in/ryan-martin-8909ab126/" target="_blank">
       <img src="${linkedInLogo}" class="logo linkedIn" alt="LinkedIn logo" />
+    </a>
+    <a href="https://www.linkedin.com/in/ryan-martin-8909ab126/" target="_blank">
+      <img src="${githubLogo}" class="logo gitHub" alt="GitHub logo" />
     </a>
     
   </div>
