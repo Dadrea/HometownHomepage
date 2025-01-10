c1604403c5a7bc8e420070f6176d3b974c259396
Dadrea <rm4982@gmail.com>
2025-01-10 09:31:49 -0500

    fixed mobile implementation

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 import './styles.css'
 import linkedInLogo from "/linkedin.svg"
-import githubLogo from "/gihub-mark-white.svg"
+import githubLogo from "/github-mark.svg"
 
 document.querySelector('#contactMe').innerHTML = `
   <div>
     <a href="https://www.linkedin.com/in/ryan-martin-8909ab126/" target="_blank">
       <img src="${linkedInLogo}" class="logo linkedIn" alt="LinkedIn logo" />
     </a>
-    <a href="https://www.linkedin.com/in/ryan-martin-8909ab126/" target="_blank">
+    <a href="https://github.com/Dadrea" target="_blank">
       <img src="${githubLogo}" class="logo gitHub" alt="GitHub logo" />
     </a>
     
